a9704b77660daadef3f93a2416f0385237c7e5cf
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-08 00:22:33 +0900

    Modification in the Source Code

---
+++ HCKR News/Models/NetworkManager.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkManager.swift
+//  HCKR News
+//
+//  Created by Eldor Alikuvatov on 2022/11/08.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData() {
+        let url = URL(string: "")
+    }
+}
