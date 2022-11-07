3ac20c12e04e8e9e19e82d142b5f76117c440c33
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-08 00:58:55 +0900

    Update DetailView.swift

--- HCKR News/Views/DetailView.swift
+++ HCKR News/Views/DetailView.swift
@@ -12,12 +12,12 @@ struct DetailView: View {
     let url: String?
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello, World!")
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(url: "https://www.apple.com")
     }
 }
