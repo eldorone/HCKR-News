57bfaf9e5580a2dd2d7d16855225b2ee662888ba
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-07 23:21:33 +0900

    Initial Commit

---
+++ HCKR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HCKR News
+//
+//  Created by Eldor Alikuvatov on 2022/11/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HCKR News/HCKR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HCKR_NewsApp.swift
+//  HCKR News
+//
+//  Created by Eldor Alikuvatov on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct HCKR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
