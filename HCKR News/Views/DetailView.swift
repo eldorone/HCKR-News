38221c4434644413185f3e2e238066c249bdd6f1
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-08 01:23:00 +0900

    modification in the source code

--- HCKR News/Views/DetailView.swift
+++ HCKR News/Views/DetailView.swift
@@ -27,11 +27,16 @@ struct WebView: UIViewRepresentable {
     
     let urlString: String?
     
-    func makeUIView(context: UIViewRepresentableContext<WebView>) -> WebView.UIViewType {
+    func makeUIView(context: Context) -> WebView.UIViewType {
         return WKWebView()
     }
     
-    func updateUIView(_ uiView: WebView.UIViewType, context: UIViewControllerRepresentableContext<WebView>) {
-        <#code#>
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
     }
 }
