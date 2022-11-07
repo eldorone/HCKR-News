ef1f167b81ef9dfb9660510b75481700ac42b768
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-08 00:57:25 +0900

    modification in the source code

---
+++ HCKR News/Views/DetailView.swift
@@ -0,0 +1,23 @@
+//
+//  DetailView.swift
+//  HCKR News
+//
+//  Created by Eldor Alikuvatov on 2022/11/08.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
